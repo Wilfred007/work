dc97ab35258ffecc194adc478204125493dea70d
Wilfred007 <adzerwilfred007@gmail.com>
2024-05-01 03:30:25 -0700

    buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 
 
@@ -11,8 +13,13 @@ export default function Home () {
       </h1>
       <p className="mt-6 text-lg max-w-prose text-muted-foreground">Experience lightning-fast, reliable internet services that seamlessly connect you to the digital world.
       </p>
-      <p className="mt-5 text-lg max-w-prose text-muted-foreground">With over 5 years of proven expertise, we stand as your top choice for cutting-edge online connectivity solutions.
+      <p className="mt-4 text-lg max-w-prose text-muted-foreground">With over 5 years of proven expertise, we stand as your top choice for cutting-edge online connectivity solutions.
     </p>
+    <div className="flex flex-col sm:flex-row gap-4 mt-6">
+      <Link href="/" className={buttonVariants()}>Sign Up</Link>
+      <Button variant='ghost'>Login &rarr;</Button>
+
+    </div>
       </div>
     </MaxWidthWrapper>
   )
