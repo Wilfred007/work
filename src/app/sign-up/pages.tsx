c002dae20f55c4a33256a4308ef5f8dd7df0f332
Wilfred007 <adzerwilfred007@gmail.com>
2024-05-01 22:21:22 -0700

    signup

--- src/app/sign-up/pages.tsx
+++ src/app/sign-up/pages.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
-import { Button, buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils"
 import React from 'react'
 
